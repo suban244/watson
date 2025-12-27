20b35072d185b012824dfe67b5c8586418ff8e31
suban244 <subanshrestha244@gmail.com>
2025-12-27 23:02:24 +0545

    update psycopg dependency

--- backend/celery/worker/start.sh
+++ backend/celery/worker/start.sh
@@ -6,4 +6,4 @@ set -o nounset
 
 export PYTHONPATH=/app/app
 cd /app/app
-exec /app/.venv/bin/celery -A celery_app.celery_app worker -l INFO
+exec /app/.venv/bin/celery -A celery_app.celery_app worker -l INFO gevent -c 50

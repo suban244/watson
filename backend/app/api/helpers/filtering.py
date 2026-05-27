026192e92b780be5e49844b7611e38a72b2b96b4
Suban Shrestha <subanshrestha244@gmail.com>
2026-05-27 23:09:52 +0545

    :wrench: add pg_search for elastic search capabilities

--- backend/app/api/helpers/filtering.py
+++ backend/app/api/helpers/filtering.py
@@ -4,19 +4,25 @@ from fastapi import Query
 from db.models import Transaction
 from sqlalchemy.sql.elements import ColumnElement
 
+
 class TransactionFilter(BaseModel):
     categories: list[str] | None = None
+
     date_from: date | None = None
     date_to: date | None = None
+
     is_expense: bool | None = None
+    amount_min: float | None = None
+    amount_max: float | None = None
 
     @classmethod
     def get_filterset(
         cls,
+        *,
         categories: list[str] = Query(
             None, description="Filter transactions by categories"
         ),
-        date_from: date  = Query(
+        date_from: date = Query(
             None, description="Filter transactions from this date (YYYY-MM-DD)"
         ),
         date_to: date = Query(
@@ -25,12 +31,22 @@ class TransactionFilter(BaseModel):
         is_expense: bool = Query(
             None, description="Filter transactions by expense type"
         ),
+        amount_min: float = Query(
+            None,
+            description="Filter transactions with amount greater than or equal to this value",
+        ),
+        amount_max: float = Query(
+            None,
+            description="Filter transactions with amount less than or equal to this value",
+        ),
     ):
         return cls(
             categories=categories,
             date_from=date_from,
             date_to=date_to,
             is_expense=is_expense,
+            amount_min=amount_min,
+            amount_max=amount_max,
         )
 
     def get_conditions(self) -> list[ColumnElement[bool]]:
@@ -43,4 +59,8 @@ class TransactionFilter(BaseModel):
             conditions.append(Transaction.date <= self.date_to)
         if self.is_expense is not None:
             conditions.append(Transaction.is_expense == self.is_expense)
+        if self.amount_min is not None:
+            conditions.append(Transaction.amount >= self.amount_min)
+        if self.amount_max is not None:
+            conditions.append(Transaction.amount <= self.amount_max)
         return conditions
